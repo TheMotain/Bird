3f8134d887a01959a0d474e4bb0e46b18ceeeff2
Alex Dalencourt <alex.dalencourt@yahoo.fr>
2015-01-20 16:33:29 +0100

    développement des socket

--- webserver/main.c
+++ webserver/main.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <string.h>
-int main(int argc,char **argv)
+#include "socket.h"
+#include <unistd.h>       
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+int main()
 {
-	/* Arnold Robbins in the LJ of February '95, describing RCS */
-	if (argc > 1 && strcmp(argv[1], "-advice") == 0) {
-		printf("Don't Panic!\n");
-		return 42;
-	}
-	printf("Need an advice?\n");
-	return 0;
+  int socket_client;
+  int socket_serveur;
+  socket_serveur = creer_serveur(8000);
+  socket_client = accept(socket_serveur, NULL, NULL);
+  if(socket_client == -1)
+    {
+      perror("accept");
+      return 0;
+    }
+
+  const char *message = "@@@@@@@@@@@@@@@@@@@@@**^^\"\"~~~\"^@@^*@*@@**@@@@@@@@@\n@@@@@@@@@@@@@*^^'\"~   , - ' '; ,@@b. '  -e@@@@@@@@@\n@@@@@@@@*^\"~      . '     . ' ,@@@@(  e@*@@@@@@@@@@\n@@@@@^~         .       .   ' @@@@@@, ~^@@@@@@@@@@@\n@@@~ ,e**@@*e,  ,e**e, .    ' '@@@@@@e,  \"*@@@@@'^@\n@',e@@@@@@@@@@ e@@@@@@       ' '*@@@@@@    @@@'   0\n@@@@@@@@@@@@@@@@@@@@@',e,     ;  ~^*^'    ;^~   ' 0\n@@@@@@@@@@@@@@@^""^@@e@@@   .'           ,'   .'  @\n@@@@@@@@@@@@@@'    '@@@@@ '         ,  ,e'  .    ;@\n@@@@@@@@@@@@@' ,&&,  ^@*'     ,  .  i^\"@e, ,e@e  @@\n@@@@@@@@@@@@' ,@@@@,          ;  ,& !,,@@@e@@@@ e@@\n@@@@@,~*@@*' ,@@@@@@e,   ',   e^~^@,   ~'@@@@@@,@@@\n@@@@@@, ~\" ,e@@@@@@@@@*e*@*  ,@e  @@""@e,,@@@@@@@@@\n@@@@@@@@ee@@@@@@@@@@@@@@@\" ,e@' ,e@' e@@@@@@@@@@@@@\n@@@@@@@@@@@@@@@@@@@@@@@@\" ,@\" ,e@@e,,@@@@@@@@@@@@@@\n@@@@@@@@@@@@@@@@@@@@@@@~ ,@@@,,0@@@@@@@@@@@@@@@@@@@\n@@@@@@@@@@@@@@@@@@@@@@@@,,@@@@@@@@@@@@@@@@@@@@@@@@@";
+  write(socket_client, message, sizeof(message));
+  return 0;
 }
+
+
--- webserver/socket.c
+++ webserver/socket.c
@@ -1,13 +1,35 @@
 #include "socket.h"
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <stdio.h>
 
 int creer_serveur(int port)
 {
-	int socket_serveur;
-	socket_serveur = socket(AF_INET,SOCK_STREAM,0);
-	if(socket_serveur == -1)
-	{
-		perror("socket_serveur");
-		exit(-1);
-	}
-	return 0;
+  int socket_serveur;
+  struct sockaddr_in saddr;
+	
+  socket_serveur = socket(AF_INET,SOCK_STREAM,0);
+  if(socket_serveur == -1)
+    {
+      perror("socket_serveur");
+      exit(-1);
+    }
+
+  saddr.sin_family = AF_INET; /* Socket ipv4 */
+  saddr.sin_port = htons(port); /* Port d'écoute */
+  saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
+  if(bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1)
+    {
+      perror("bind socket_serveur");
+      exit(-1);
+    }
+
+  if(listen(socket_serveur,10) == -1)
+    {
+      perror("listen socket_serveur");
+      exit(-1);
+    } 
+  return socket_serveur;
 }
