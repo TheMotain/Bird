93436b10cf018ae44cfbd8f94a780ce85c53556f
Cyprien Desquiens <cyprien.desquiens@laposte.net>
2015-01-26 15:51:57 +0100

    ajout de l'option setsockopt

--- webserver/socket.c
+++ webserver/socket.c
@@ -7,29 +7,37 @@
 
 int creer_serveur(int port)
 {
-  int socket_serveur;
-  struct sockaddr_in saddr;
+	int socket_serveur;
+	int optval = 1;
+	struct sockaddr_in saddr;
 	
-  socket_serveur = socket(AF_INET,SOCK_STREAM,0);
-  if(socket_serveur == -1)
-    {
-      perror("socket_serveur");
-      exit(-1);
-    }
+	socket_serveur = socket(AF_INET,SOCK_STREAM,0);
+	if(socket_serveur == -1)
+	{
+		perror("socket_serveur");
+		exit(-1);
+	}
 
-  saddr.sin_family = AF_INET; /* Socket ipv4 */
-  saddr.sin_port = htons(port); /* Port d'écoute */
-  saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
-  if(bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1)
-    {
-      perror("bind socket_serveur");
-      exit(-1);
-    }
+	saddr.sin_family = AF_INET; /* Socket ipv4 */
+	saddr.sin_port = htons(port); /* Port d'écoute */
+	saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
 
-  if(listen(socket_serveur,10) == -1)
-    {
-      perror("listen socket_serveur");
-      exit(-1);
-    } 
-  return socket_serveur;
+	/* Permet au serveur de se redémarrer tout de suite, de réutiliser l'adresse bind */
+	if(setsockopt(socket_serveur,SOL_SOCKET,SO_REUSEADDR,&optval,sizeof(int)) == -1)
+	{
+		perror("Can not set SO_REUSEADDR option");
+	}
+
+	if(bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1)
+	{
+		perror("bind socket_serveur");
+		exit(-1);
+	}
+
+	if(listen(socket_serveur,10) == -1)
+	{
+		perror("listen socket_serveur");
+		exit(-1);
+	} 
+	return socket_serveur;
 }
